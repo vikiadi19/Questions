bf5495e3a3e59a6cc271a19221009e44de07388d
vikiadi19 <104242570+vikiadi19@users.noreply.github.com>
2022-05-25 15:58:02 -0500

    Time: 261 ms (8.76%), Space: 14.8 MB (53.72%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,58 +3,29 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-#         rows, cols = len(matrix), len(matrix[0])
-#         rowZero = False
-#         for r in range(rows):
-#             for c in range(cols):
-#                 if matrix[r][c] == 0:
-#                     if r>0:
-#                         matrix[r][0] = 0
-#                     else:
-#                         rowZero = True
-#                     matrix[0][c] = 0
+        rows, cols = len(matrix), len(matrix[0])
+        rowZero = False
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == 0:
+                    if r>0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+                    matrix[0][c] = 0
                     
-#         for r in range(1, rows):
-#             for c in range(1, cols):
-#                 if matrix[r][0] == 0 or matrix[0][c]==0:
-#                     matrix[r][c] = 0
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[r][0] == 0 or matrix[0][c]==0:
+                    matrix[r][c] = 0
                     
-#         if rowZero == True:
-#             for c in range(cols):
-#                 matrix[0][c] = 0
+        if matrix[0][0] == 0:
+            for r in range(rows):
+                matrix[r][0] = 0
+                    
+        if rowZero == True:
+            for c in range(cols):
+                matrix[0][c] = 0
                 
-#         if matrix[0][0] == 0:
-#             for r in range(rows):
-#                 matrix[r][0] = 0
+        
                 
-#         return matrix
-
-        m = len(matrix)
-        n = len(matrix[0])
-		
-        first_row_has_zero = False
-        first_col_has_zero = False
-        
-        # iterate through matrix to mark the zero row and cols
-        for row in range(m):
-            for col in range(n):
-                if matrix[row][col] == 0:
-                    if row == 0:
-                        first_row_has_zero = True
-                    if col == 0:
-                        first_col_has_zero = True
-                    matrix[row][0] = matrix[0][col] = 0
-    
-        # iterate through matrix to update the cell to be zero if it's in a zero row or col
-        for row in range(1, m):
-            for col in range(1, n):
-                matrix[row][col] = 0 if matrix[0][col] == 0 or matrix[row][0] == 0 else matrix[row][col]
-        
-        # update the first row and col if they're zero
-        if first_row_has_zero:
-            for col in range(n):
-                matrix[0][col] = 0
-        
-        if first_col_has_zero:
-            for row in range(m):
-                matrix[row][0] = 0
