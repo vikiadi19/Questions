df5bef118579dc10588faab9455460fba5345962
vikiadi19 <104242570+vikiadi19@users.noreply.github.com>
2022-05-27 09:36:00 -0500

    Time: 195 ms (46.33%), Space: 14 MB (56.90%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -0,0 +1,18 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # d, changes_allowed, maxL = {}, k, 0
+        # for i in range(len(s)):
+        #     d[s[i]] = 1 + d.get(s[i], 0)
+        # max_key = max(d, key=d.get)
+        l, r, res = 0, 0, 0
+        count = {}
+        while r<len(s):
+            count[s[r]] = 1 + count.get(s[r],0)
+            
+            while (r-l+1)-max(count.values())>k:
+                count[s[l]]-=1
+                l+=1
+            
+            res = max(res, r-l+1)
+            r+=1
+        return res
