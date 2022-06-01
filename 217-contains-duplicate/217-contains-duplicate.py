a4fa4e0d0f277b2f64eed3c05cf961fbdf028b64
vikiadi19 <104242570+vikiadi19@users.noreply.github.com>
2022-06-01 14:08:24 -0500

    Time: 581 ms (49.67%), Space: 26 MB (72.28%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -8,4 +8,15 @@ class Solution:
                 return True
             
         return False
-        
+    
+    
+    
+    #sol1 - compare set(nums) to nums, if there is difference in length then True
+        #TC - O(n), SC - O(n)
+    #sol2 - create a dictionary or frequency distribution - any element if has value greater than 1, break and return true or that key
+        #TC - O(n), SC - O(n)
+    #sol3 - sort and use two pointers side by side, if both pointers point to same value then return True and return that value.
+        #TC - O(nlogn), SC-O(1)
+    
+    
+    
