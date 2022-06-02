71327660a8a2707135a3dd16c491522173aa1e1f
vikiadi19 <104242570+vikiadi19@users.noreply.github.com>
2022-06-02 09:51:10 -0500

    Time: 225 ms (10.13%), Space: 18.4 MB (41.31%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,12 +1,34 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = defaultdict(list)
-        for i in strs:
-            count = [0]*26
-            for c in i:
-                count[ord(c)-ord("a")] += 1
+        # TC - O(len(strs)*len(string)*26)
+        # SC - O(nm)
+#         ans = defaultdict(list)
+#         for i in strs:
+#             count = [0]*26
+#             for c in i:
+#                 count[ord(c)-ord("a")] += 1
                 
-            ans[tuple(count)].append(i)
+#             ans[tuple(count)].append(i)
             
-        return ans.values()
+#         return ans.values()
                 
+        
+        
+        
+    #sol1 - we create a list 26 characters long(len of alphabets)
+    #       and a default dict of list as ans
+    #       we loop each word and inside each word we loop the character and based on its 
+    #       ord value - ord value of a we assign the value at particular index in list
+    #       we use this list as key for dictionary and append any word that has the same key
+    #       we cannot have list as key in dictionary therefore we convert it into tuple first
+    #       we return the values.
+    #   TC-0(n*k), SC-0(n*k)
+    
+    #sol2 - use sort to use sorted word as key for dictionary
+    #       
+        d = defaultdict(list)
+        for word in strs:
+            key = tuple(sorted(word))
+            d[key].append(word)
+            
+        return d.values()
