ee1add64c46bb3ced8a168e48872a539573aed63
vikiadi19 <104242570+vikiadi19@users.noreply.github.com>
2022-06-08 09:04:20 -0500

    Time: 259 ms (10.15%), Space: 14.7 MB (89.31%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,20 +3,34 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rowZero, colZero = set(), set()
+        # rowZero, colZero = set(), set()
+        firstCol = 1
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
+                # if matrix[i][0] == 0:
+                    # firstCol = 0
                 if matrix[i][j] == 0:
-                    rowZero.add(i)
-                    colZero.add(j)
+                    if j>0:
+                        matrix[0][j] = 0
+                    else:
+                        firstCol = 0
+                    matrix[i][0] = 0
                     
-        for i in rowZero:
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
+                    
+        if matrix[0][0] == 0:
             for j in range(len(matrix[0])):
-                matrix[i][j] = 0
-                
-        for j in colZero:
+                matrix[0][j] = 0
+        
+        if firstCol == 0:
             for i in range(len(matrix)):
-                matrix[i][j] = 0
-                
+                matrix[i][0] = 0
+        
         return matrix
-                
+            
+        print(matrix)
+        
+        
