7de6ef35ac9a3f27fd4fd643350c168f9c3f84ad
vikiadi19 <104242570+vikiadi19@users.noreply.github.com>
2022-06-21 06:08:16 -0500

    Time: 155 ms (62.62%), Space: 15 MB (62.50%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -6,30 +6,32 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if root == None: return False
-        if subRoot == None: return True
-        
+        if (subRoot == None):
+            return True
         if root == None:
             return False
         
+        if self.sameTree(root, subRoot) == True:
+                return True
         
-        if self.sameTree(root, subRoot):
-            return True
-        else:
-            return (self.isSubtree(root.left, subRoot) or
-            self.isSubtree(root.right, subRoot))
-    
-    
+        x = self.isSubtree(root.left, subRoot)
+        y = self.isSubtree(root.right, subRoot)
+        
+        return x or y
+        
+        
+        
     def sameTree(self, p, q):
         if p == None and q == None:
             return True
+
         if p == None or q == None:
             return False
-        
-        if p.val!=q.val:
+
+        if p.val != q.val:
             return False
-        
-        val = self.sameTree(p.left, q.left) and self.sameTree(p.right, q.right)
-        return val
-        
+
+        lt = self.sameTree(p.left, q.left)
+        rt = self.sameTree(p.right, q.right)
+        return lt and rt
         
