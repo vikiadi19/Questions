04c4b4f4e8e61c4e10c7970e923426edd603d282
vikiadi19 <104242570+vikiadi19@users.noreply.github.com>
2022-07-11 18:39:03 -0500

    Time: 464 ms (53.59%), Space: 16.5 MB (63.90%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,23 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        l, m, count = len(grid), len(grid[0]), 0
-        visited = [[False for j in range(m)] for i in range(l)]
+        def move(grid, visited, i, j):
+            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or visited[i][j] == True or grid[i][j] == "0":
+                return
+            
+            visited[i][j] = True
+            
+            move(grid, visited, i-1, j)
+            move(grid, visited, i+1, j)
+            move(grid, visited, i, j-1)
+            move(grid, visited, i, j+1)
         
-        for i in range(l):
-            for j in range(m):
+        
+        visited = [[False for j in range(len(grid[0]))] for i in range(len(grid))]
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == "1" and visited[i][j] == False:
                     count += 1
-                    self.graph(grid, i, j, visited)
+                    move(grid, visited, i, j)
+                    
                     
         return count
         
-    def graph(self, grid, i, j, visited):
         
-        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or visited[i][j] == True or grid[i][j] == "0":
-            return
         
-        visited[i][j] = True
-        self.graph(grid, i-1, j, visited)
-        self.graph(grid, i+1, j, visited)
-        self.graph(grid, i, j-1, visited)
-        self.graph(grid, i, j+1, visited)
