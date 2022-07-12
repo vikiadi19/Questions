2fd76b11087a0035bc2e1a36796747354b1190bb
vikiadi19 <104242570+vikiadi19@users.noreply.github.com>
2022-07-11 23:38:22 -0500

    Time: 156 ms (33.12%), Space: 14.1 MB (28.28%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,32 +1,36 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        if target<0:
-            return []
-        if target == 0:
-            return [[]]
+        # if target<0:
+        #     return []
+        # if target == 0:
+        #     return [[]]
+        # res = []
+        # for i in range(len(candidates)):
+        #     combos = self.combinationSum(candidates[i:], target-candidates[i])
+        #     if type(combos) == list:
+        #         for combo in combos:
+        #             combo.append(candidates[i])
+        #             res.append(combo)
+        #         # print(res)
+        # return res
+
+
+
+
         res = []
-        for i in range(len(candidates)):
-            combos = self.combinationSum(candidates[i:], target-candidates[i])
-            if type(combos) == list:
-                for combo in combos:
-                    combo.append(candidates[i])
-                    res.append(combo)
-                # print(res)
-        return res
+        def lno(candidates, target, asf):
+            if target<0:
+                return 
+            if target==0:
+                res.append(asf.copy())
+                # asf.clear()
+                return
 
-
-
-
-# res = []
-#         def lno(candidates, target, asf):
-#             if target<0:
-#                 return 
-#             if target==0:
-#                 res.append(asf)
-#                 return
-            
-#             for i in candidates:
-#                 lno(candidates, target-i, asf.append(i))
+            for i in range(len(candidates)):
+                asf.append(candidates[i])
+                lno(candidates[i:], target-candidates[i], asf)   
+                asf.pop()
                 
-#         lno(candidates, target, [])   
-#         return res
+        asf = []
+        lno(candidates, target, asf)   
+        return res
