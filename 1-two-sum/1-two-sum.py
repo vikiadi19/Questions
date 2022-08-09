abb1e39f071b16c0dd95fdef72e42b17e9513186
vikiadi19 <104242570+vikiadi19@users.noreply.github.com>
2022-08-08 22:37:45 -0500

    Time: 68 ms (90.48%), Space: 15.3 MB (24.32%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -2,8 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
         for i in range(len(nums)):
-            if nums[i] in d:
-                return [i, d[nums[i]]]
+            if target-nums[i] in d:
+                return [i, d[target-nums[i]]]
             else:
-                d[target-nums[i]] = i
-                
+                d[nums[i]] = i
